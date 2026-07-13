341647f012636dd0ef65bd92b9187c819d420afc
Bhuvan <bhuvanaadarapu@gmail.com>
2026-07-13 10:31:31 +0530

    second commit

--- index.js
+++ index.js
@@ -12,7 +12,21 @@ app.get("/",(req, res) => {
     res.send("Hello CSE-A...")
 });
 
+app.get('/search', (req, res) => {
+	const { keyword, type } = req.query;
+	res.send(`Search Query Received - Keyword: ${keyword}, Type: ${type}`); 
+});
+
+app.get('/redirect-to-user/:name', (req, res) => {
+	const name = req.params.name; // Build a new URL to redirect
+	const userProfileURL = `/profile/${name}?details=full`; res.redirect(userProfileURL);
+});
+
+app.get('/profile/:name', (req, res) => {
+	const name = req.params.name; const details = req.query.details;
+	res.send(`Profile Page of ${name} - Details: ${details}`); 
+});
+
 app.listen(PORT, () => {
    console.log(`Server is running at http://localhost:${PORT}`); 
 })
-
